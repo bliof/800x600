5a44e903e226e806978369fdd31f15824422aa3e
Aleksandar Ivanov <aivanov92@gmail.com>
2014-12-28 01:22:10 +0200

    Add larger original for the extension icon v1 :) and specify 19x19 & 38x38

--- toggle.js
+++ toggle.js
@@ -2,10 +2,22 @@ var activeTabs = {};
 
 chrome.browserAction.onClicked.addListener(function (tab) {
     if (activeTabs[tab.id]) {
-        chrome.browserAction.setIcon({path: 'inactive.png', tabId: tab.id});
+        chrome.browserAction.setIcon({
+            path: {
+                '19': 'images/inactive19.png',
+                '38': 'images/inactive38.png'
+            },
+            tabId: tab.id
+        });
         delete activeTabs[tab.id];
     } else {
-        chrome.browserAction.setIcon({path: 'active.png', tabId: tab.id});
+        chrome.browserAction.setIcon({
+            path: {
+                '19': 'images/active19.png',
+                '38': 'images/active38.png'
+            },
+            tabId: tab.id
+        });
         activeTabs[tab.id] = true;
     }
 });
