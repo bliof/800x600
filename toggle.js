de515a4fd785f297a10d01ce9bdf46e5b186a2c9
Aleksandar Ivanov <aivanov92@gmail.com>
2015-01-01 14:27:30 +0200

    Move into functions the logic for activating and deactivating the plugin on tabs

--- toggle.js
+++ toggle.js
@@ -1,27 +1,35 @@
-var activeTabs = {};
+var activeOnTabs = {};
 
-chrome.browserAction.onClicked.addListener(function (tab) {
-    if (activeTabs[tab.id]) {
-        chrome.tabs.sendMessage(tab.id, {action: "undo-swap-images"});
+function activateOnTab(tab) {
+    chrome.tabs.sendMessage(tab.id, {action: "swap-images"});
 
-        chrome.browserAction.setIcon({
-            path: {
-                '19': 'images/inactive19.png',
-                '38': 'images/inactive38.png'
-            },
-            tabId: tab.id
-        });
-        delete activeTabs[tab.id];
+    chrome.browserAction.setIcon({
+        path: {
+            '19': 'images/active19.png',
+            '38': 'images/active38.png'
+        },
+        tabId: tab.id
+    });
+    activeOnTabs[tab.id] = true;
+}
+
+function deactivateOnTab(tab) {
+    chrome.tabs.sendMessage(tab.id, {action: "undo-swap-images"});
+
+    chrome.browserAction.setIcon({
+        path: {
+            '19': 'images/inactive19.png',
+            '38': 'images/inactive38.png'
+        },
+        tabId: tab.id
+    });
+    delete activeOnTabs[tab.id];
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    if (activeOnTabs[tab.id]) {
+        deactivateOnTab(tab);
     } else {
-        chrome.tabs.sendMessage(tab.id, {action: "swap-images"});
-
-        chrome.browserAction.setIcon({
-            path: {
-                '19': 'images/active19.png',
-                '38': 'images/active38.png'
-            },
-            tabId: tab.id
-        });
-        activeTabs[tab.id] = true;
+        activateOnTab(tab);
     }
 });
