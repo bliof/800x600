c656338fb5dc060f50c29f0d31c8f1c4b0f52909
Aleksandar Ivanov <aivanov92@gmail.com>
2015-01-01 15:54:42 +0200

    Check in the content script if the swapping is active on the current tab and run it

--- swap.js
+++ swap.js
@@ -1,31 +1,37 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    function swapImage(img) {
-        var realSrc = img.src;
-        img.src = 'http://dummyimage.com/' + img.naturalWidth + 'x' + img.naturalHeight;
-        img.classList.add('_dummy_');
-        img.dataset.realSrc = realSrc;
-    }
+function swapImage(img) {
+    var realSrc = img.src;
+    img.src = 'http://dummyimage.com/' + img.naturalWidth + 'x' + img.naturalHeight;
+    img.classList.add('_dummy_');
+    img.dataset.realSrc = realSrc;
+}
 
-    function startSwapping() {
-        var images = document.querySelectorAll('img');
+function startSwapping() {
+    var images = document.querySelectorAll('img');
 
-        for (var i = 0; i < images.length; i++) {
-            var img = images[i];
+    for (var i = 0; i < images.length; i++) {
+        var img = images[i];
 
-            if (img.naturalWidth === 0) {
-                img.addEventListener('load', function imageSwapHandler() {
-                    swapImage(img);
-                    img.removeEventListener(img, imageSwapHandler);
-                });
-            } else {
+        if (img.naturalWidth === 0) {
+            img.addEventListener('load', function imageSwapHandler() {
                 swapImage(img);
-            }
+                img.removeEventListener(img, imageSwapHandler);
+            });
+        } else {
+            swapImage(img);
         }
     }
+}
 
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action == 'swap-images') {
         startSwapping();
     } else {
         window.location.reload();
+    }
+});
+
+chrome.runtime.sendMessage({action: 'init'}, function(response) {
+    if (response.action == 'swap-images') {
+        startSwapping();
     }
 });
--- toggle.js
+++ toggle.js
@@ -1,28 +1,37 @@
 var activeOnTabs = {};
 
+function changeIconTo(tab, state) {
+    var icon = {};
+
+    if (state == 'active') {
+        icon = {
+            '19': 'images/active19.png',
+            '38': 'images/active38.png'
+        }
+    } else {
+        icon = {
+            '19': 'images/inactive19.png',
+            '38': 'images/inactive38.png'
+        };
+    }
+
+    chrome.browserAction.setIcon({
+        path: icon,
+        tabId: tab.id
+    });
+}
+
 function activateOnTab(tab) {
     chrome.tabs.sendMessage(tab.id, {action: "swap-images"});
 
-    chrome.browserAction.setIcon({
-        path: {
-            '19': 'images/active19.png',
-            '38': 'images/active38.png'
-        },
-        tabId: tab.id
-    });
+    changeIconTo(tab, 'active');
     activeOnTabs[tab.id] = true;
 }
 
 function deactivateOnTab(tab) {
     chrome.tabs.sendMessage(tab.id, {action: "undo-swap-images"});
 
-    chrome.browserAction.setIcon({
-        path: {
-            '19': 'images/inactive19.png',
-            '38': 'images/inactive38.png'
-        },
-        tabId: tab.id
-    });
+    changeIconTo(tab, 'inactive');
     delete activeOnTabs[tab.id];
 }
 
@@ -31,5 +40,16 @@ chrome.browserAction.onClicked.addListener(function(tab) {
         deactivateOnTab(tab);
     } else {
         activateOnTab(tab);
+    }
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    var tab = sender.tab;
+
+    if (request.action == 'init') {
+        if (activeOnTabs[tab.id]) {
+            changeIconTo(tab, 'active');
+            sendResponse({action: 'swap-images'})
+        }
     }
 });
